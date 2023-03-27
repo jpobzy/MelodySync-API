2f794619618e5670bd9c337b84c539dc8cc8449e
Joey <77253943+jpobzy@users.noreply.github.com>
2023-03-27 08:37:11 -0400

    Update spotify_innit_tests.py

--- spotify/tests/spotify_innit_tests.py
+++ spotify/tests/spotify_innit_tests.py
@@ -1,6 +1,6 @@
 class spotify_innit_tests():
     def run_tests(spotify):
-        print("BEGINNING TEST RUNS FOR spotifyAPI")
+        # print("BEGINNING TEST RUNS FOR spotifyAPI")
         expected_display_name = "JPobs_"
         expected_id = "nnnp2au6xj317cjmxn9jsuxlj"
         expected_image_url = "https://i.scdn.co/image/ab6775700000ee85e93208cee96d61db701839a2"
@@ -9,5 +9,5 @@ class spotify_innit_tests():
         assert spotify.get_id() == expected_id, f"GET DISPLAY NAMES DID NOT MATCH. Expected: {expected_display_name}, Actual: {spotify.get_id()}"
         assert spotify.get_profile_image_url() == expected_image_url, f"GET DISPLAY NAMES DID NOT MATCH. Expected: {expected_display_name}, Actual: {spotify.get_profile_image_url()}"
         assert spotify.get_uri() == expected_uri, f"GET DISPLAY NAMES DID NOT MATCH. Expected: {expected_display_name}, Actual: {spotify.get_uri()}"
-        print("ALL TESTS COMPLETED SUCCESSFULLY")  
+
 
