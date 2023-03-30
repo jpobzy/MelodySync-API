025bef062a61a8c81c012568d06fbfa17e4575fa
Joey <77253943+jpobzy@users.noreply.github.com>
2023-03-29 20:39:07 -0400

    Update playlist.py

--- spotify/playlist/playlist.py
+++ spotify/playlist/playlist.py
@@ -1,6 +1,20 @@
-class playlist:
+class Playlist:
     def __init__(self, playlist):
-        self.playlist = playlist
+        self.collaborative = playlist['collaborative']
+        self.description = playlist['description']
+        self.external_urls = playlist['external_urls']
+        self.href = playlist['href']
+        self.id = playlist['id']
+        self.images = playlist['images']
+        self.name = playlist['name']
+        self.owner = playlist['owner']
+        self.primary_color = playlist['primary_color']
+        self.public = playlist['public']
+        self.snapshot_id = playlist['snapshot_id']
+        self.tracks = playlist['tracks']
+        self.type = playlist['type']
+        self.uri = playlist['uri']
+
     
-    def test(self):
-        pass
+    
+    
